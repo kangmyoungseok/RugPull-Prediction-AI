1e89ecfa23c94cf4c5b74b1c2fd667752445d330
Myeongseokkang <kms00129@naver.com>
2021-11-10 20:58:14 +0900

    정상 토큰의 TImestamp 변경작업

---
+++ 2. Feature/FixTimestamp/Receiver_reduce.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from pandas.core.frame import DataFrame
+datas = pd.read_csv('Labeling_v1.5.csv',encoding='utf-8-sig').to_dict('records')
+len(datas)
+
+
+result = []
+for data in datas:
+    if(len(data['receiver_list']) > 1 ):
+        if(data['token00_creator_address'] in data['receiver_list']):
+            print(data['token00_creator_address'])
+            data['receiver_list'] = data['token00_creator_address']
+            result.append(data)
+            continue
+        if(data['LP_Creator_address'] in data['receiver_list']):
+            data['receiver_list'] = data['LP_Creator_address']
+            print(data['LP_Creator_address'])
+            result.append(data)
+            continue
+    else:
+        data['receiver_list'] = data['receiver_list'][0]
+        result.append(data)
+print(len(result))
+
+df = pd.DataFrame(result)
+df.to_csv('Labeling_v1.6.csv',encoding='utf-8-sig',index=False)
